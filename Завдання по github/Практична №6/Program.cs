415338f4137d77a61bb72bbe596fa3572c37cf91
SerhiiTarhonskyi <118011647+SerhiiTarhonskyi@users.noreply.github.com>
2023-04-29 17:33:03 +0300

    Add files via upload

---
+++ Завдання по github/Практична №6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A, B;
+
+        Console.Write("Введіть число A: ");
+        A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть число B: ");
+        B = int.Parse(Console.ReadLine());
+
+        int phiA = totient(A);
+        int phiB = totient(B);
+
+        int count = phiA * phiB;
+
+        Console.WriteLine("Кількість пар взаємно простих чисел: " + count);
+    }
+
+    static int totient(int n)
+    {
+        int result = n;
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                while (n % i == 0)
+                {
+                    n /= i;
+                }
+
+                result -= result / i;
+            }
+        }
+
+        if (n > 1)
+        {
+            result -= result / n;
+        }
+
+        return result;
+    }
+}
